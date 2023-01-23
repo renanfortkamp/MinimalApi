f7e73ea7d114a5a7b2fb2cf269196d2049a30e92
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-23 20:22:10 -0300

    injecting dependency in program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MinimalApiContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("ServerConnection")));
 
 var app = builder.Build();
 
