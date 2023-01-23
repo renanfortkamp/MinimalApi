d02d0cd40b8072216af98d0b8948171cba87233e
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-23 20:05:55 -0300

    Add Entiti Usuario

---
+++ Models/Entities/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MinimalApi.Models.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Genero { get; set; }
+        public string Cpf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+
+    }
+}
