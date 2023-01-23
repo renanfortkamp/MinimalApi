edfd03eddc70a873a6940ed6ba4823be83a5907a
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-23 20:17:45 -0300

    Add Class Context

---
+++ Context/MinimalApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalApi.Models.Entities;
+
+namespace MinimalApi.Context
+{
+    public class MinimalApiContext : DbContext
+    {
+        public MinimalApiContext(DbContextOptions<MinimalApiContext> options): base(options)
+        {
+        }
+
+        public DbSet<Usuario> UsuarioDbSet { get; set; }
+    }
+}
