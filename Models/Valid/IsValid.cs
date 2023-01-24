e9ea2858440b141fd1e38722a65d1df8f31d8d32
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-23 22:40:38 -0300

    validation class with cpf validation method

---
+++ Models/Valid/IsValid.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+
+namespace MinimalApi.Models.Valid
+{
+    public class IsValid
+    {
+      
+        public static bool IsValidCPF(string cpf)
+                {
+                    bool isValid = true;
+                    if (string.IsNullOrWhiteSpace(cpf) || cpf.Length != 11)
+                    {
+                        isValid = false;
+                    }
+                    else
+                    {
+                        for (int i = 0; i < cpf.Length; i++)
+                        {
+                            if (!Char.IsDigit(cpf[i]))
+                            {
+                                isValid = false;
+                                break;
+                            }
+                        }
+                    }
+                    return isValid;
+                }
+    }
+}
