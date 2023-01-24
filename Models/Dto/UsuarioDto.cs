7898b10e584b9222bc796ac4543c6bee1c74f1bb
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-23 23:51:20 -0300

    add dto to return only full name and date of birth and dto to change email and password

---
+++ Models/Dto/UsuarioDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MinimalApi.Models.Entities;
+
+namespace MinimalApi.Models.Dto
+{
+    public class UsuarioDto
+    {
+        public string NomeCompleto { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+
+        public UsuarioDto(Usuario usuario)
+        {
+            NomeCompleto = $"{usuario.Nome} {usuario.SobreNome}";
+        }
+
+
+    }
+}
