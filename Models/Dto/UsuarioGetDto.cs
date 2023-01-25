3aba3f965310337fc5170484fc4bd88d0f0422c5
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2023-01-24 22:49:25 -0300

    del unn notation and conca first and last name

--- Models/Dto/UsuarioGetDto.cs
+++ Models/Dto/UsuarioGetDto.cs
@@ -5,25 +5,9 @@ namespace MinimalApi.Models.Dto
 {
     public class UsuarioGetDto
     {
-        [Key]
-        public int Id { get; set; }
-
-        [Required]
-        [StringLength(100, ErrorMessage = "Nome precisa ter entre 3 a 100 caracteres.", MinimumLength = 3)]
-        [DataType(DataType.Text)]
-        public string Nome { get; set; }
-
-        [Required]
-        [StringLength(200, ErrorMessage = "Sobrenome precisa ter entre 3 a 200 caracteres.", MinimumLength = 3)]
-        [DataType(DataType.Text)]
-        public string SobreNome { get; set; }
-
-        [Required]
-        [DataType(DataType.Date, ErrorMessage = "Digite uma data valida. Ex:AAAA-MM-DD(1993-04-02)")]
-        public DateTime DataNascimento { get; set; }
-
-        [StringLength(100, ErrorMessage = "Telefone precisa ter entre 5 e 100 Caracteres.", MinimumLength = 5)]
-        [DataType(DataType.PhoneNumber)]
+        public string NomeCompleto { get; set; }
+        public string DataNascimento { get; set; }
         public string Telefone { get; set; }
+        public string Email { get; set; }
     }
 }
